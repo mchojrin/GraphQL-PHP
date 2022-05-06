9e1b7b88760106a2dd9fe46385422390b82d6db0
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-05-06 14:48:01 +0200

    Simplified the example to use an array as storage

--- application/public/graphql.php
+++ application/public/graphql.php
@@ -8,19 +8,111 @@ use GraphQL\Server\StandardServer;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Schema;
-use Model\User;
+
+$countries = [
+    [
+        'id' => '1ae29d',
+        'name' => 'Spain',
+    ],
+    [
+        'id' => '34ffa0',
+        'name' => 'USA',
+    ],
+    [
+        'id' => '11b3e4',
+        'name' => 'Australia',
+    ],
+];
+
+$users = [
+    [
+        'id' => 'aa32fd',
+        'name' => 'Mauro',
+        'email' => 'mauro.chojrin@leewayweb.com',
+        'birthday' => '1977-12-22',
+        'countryId' => '1ae29d',
+        'gender' => 'M',
+    ],
+    [
+        'id' => 'de14e5',
+        'name' => 'Tamara',
+        'email' => 'tlopez@gmail.com',
+        'birthday' => '1982-08-19',
+        'countryId' => '1ae29d',
+        'gender' => 'F',
+    ],
+    [
+        'id' => 'bad31',
+        'name' => 'Peter',
+        'email' => 'peter.loffatti@hotmail.com',
+        'birthday' => '1968-11-02',
+        'countryId' => '34ffa0',
+        'gender' => 'M',
+    ],
+];
+
+$countryType = new ObjectType(
+    [
+        'name' => 'Country',
+        'fields' => [
+            'id' => [
+                'type' => Type::id(),
+            ],
+            'name' => [
+                'type' => Type::string(),
+            ],
+        ]
+    ]
+);
+
+$userType = new ObjectType(
+    [
+        'name' => 'User',
+        'fields' => [
+            'id' => [
+                'type' => Type::id(),
+            ],
+            'name' => [
+                'type' => Type::string(),
+            ],
+            'email' => [
+                'type' => Type::string(),
+            ],
+            'country' => [
+                'type' => $countryType
+            ],
+            'gender' => [
+                'type' => Type::string(),
+            ],
+        ],
+    ]
+);
 
 $queryType = new ObjectType([
         'name' => 'Query',
         'fields' => [
-            'name' => [
-                'type' => Type::string(),
+            'User' => [
+                'type' => $userType,
                 'args' => [
                     'id' => [
-                        'type' => Type::string(),
+                        'type' => Type::id(),
                     ]
                 ],
-                'resolve' => fn($rootValue, array $args): User => User::findById($args['id'])
+                'resolve' => function ($rootValue, array $args) use ($users, $countries): array {
+                    $theUser = array_filter($users, function($user) use ($args) {
+
+                        return $args['id'] == $user['id'];
+                    })[0];
+
+                    $theCountry = array_filter($countries, function($country) use ($theUser) {
+
+                        return $country['id'] == $theUser['countryId'];
+                    })[0];
+
+                    $theUser['country'] = $theCountry;
+
+                    return $theUser;
+                },
             ]
         ]
     ]
